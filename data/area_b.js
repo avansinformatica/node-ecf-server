b099abfbbea32216e89983f66e0178f3d187a608
Robin Schellius <r.schellius@avans.nl>
2017-04-14 10:25:33 +0200

    updated

--- data/area_b.js
+++ data/area_b.js
@@ -50,6 +50,105 @@ var proficiency_B_2_4 = {
 	"description" : "Nog invullen"
 }
 
+
+var dimensionB3 = { 
+	"level": 3,
+	"competence": "Testing",
+	"description": "Nog invullen"
+}
+
+var proficiency_B_3_1 = {
+	"level": 1,
+	"description": "Nog invullen",
+}
+
+var proficiency_B_3_2 = { 
+	"level": 2,
+	"description": "Nog invullen",
+}
+
+var proficiency_B_3_3 = { 
+	"level": 3,
+	"description": "Nog invullen",
+}
+
+var proficiency_B_3_4 = { 
+	"level": 4,
+	"description" : "Nog invullen"
+}
+
+
+
+var dimensionB4 = { 
+	"level": 4,
+	"competence": "Solution Deployment",
+	"description": "Nog invullen"
+}
+
+var proficiency_B_4_1 = {
+	"level": 1,
+	"description": "Nog invullen",
+}
+
+var proficiency_B_4_2 = { 
+	"level": 2,
+	"description": "Nog invullen",
+}
+
+var proficiency_B_4_3 = { 
+	"level": 3,
+	"description": "Nog invullen",
+}
+
+
+
+var dimensionB5 = { 
+	"level": 5,
+	"competence": "Documentation Production",
+	"description": "Nog invullen"
+}
+
+var proficiency_B_5_1 = {
+	"level": 1,
+	"description": "Nog invullen",
+}
+
+var proficiency_B_5_2 = { 
+	"level": 2,
+	"description": "Nog invullen",
+}
+
+var proficiency_B_5_3 = { 
+	"level": 3,
+	"description": "Nog invullen",
+}
+
+
+
+var dimensionB6 = { 
+	"level": 6,
+	"competence": "Systems Engineering",
+	"description": "Nog invullen"
+}
+
+var proficiency_B_6_3 = { 
+	"level": 3,
+	"description": "Nog invullen",
+}
+
+var proficiency_B_6_4 = { 
+	"level": 4,
+	"description": "Nog invullen",
+}
+
+
+
+
+// We moeten een 'diepe copy' van area_b_description hebben, anders
+// passen we het oorspronkelijke object aan. 
+var area_b = JSON.parse(JSON.stringify(area_b_description));
+
+
 // We moeten een 'diepe copy' van area_b_description hebben, anders
 // passen we het oorspronkelijke object aan. 
 var area_b = JSON.parse(JSON.stringify(area_b_description));
@@ -60,6 +159,18 @@ area_b.dimensions = [
     },{
         dimension: dimensionB2,
         proficiency: [ proficiency_B_2_2, proficiency_B_2_3, proficiency_B_2_4]
+    },{
+        dimension: dimensionB3,
+        proficiency: [ proficiency_B_3_1, proficiency_B_3_2, proficiency_B_3_3, proficiency_B_3_4]
+    },{
+        dimension: dimensionB4,
+        proficiency: [ proficiency_B_4_1, proficiency_B_4_2, proficiency_B_4_3]
+    },{
+        dimension: dimensionB5,
+        proficiency: [ proficiency_B_5_1, proficiency_B_5_2, proficiency_B_5_3]
+    },{
+        dimension: dimensionB6,
+        proficiency: [ proficiency_B_6_3, proficiency_B_6_4]
     }]
 
 //
@@ -68,6 +179,8 @@ area_b.dimensions = [
 app.get('/', function(request, response){
     response.json(area_b);
 });
+
+// -----------------------------------------------------
 
 app.get('/1', function(request, response){
     response.json({
@@ -101,6 +214,8 @@ app.get('/1/3', function(request, response){
 	});
 });
 
+// -----------------------------------------------------
+
 app.get('/2', function(request, response){
     response.json({
 		description: area_b_description,
@@ -132,6 +247,144 @@ app.get('/2/4', function(request, response){
 		proficiency: proficiency_B_2_4
 	});
 });
+
+// -----------------------------------------------------
+
+app.get('/3', function(request, response){
+    response.json({
+		description: area_b_description,
+		dimension: dimensionB3,
+		proficiency: [ proficiency_B_3_1, proficiency_B_3_2, proficiency_B_3_3, proficiency_B_3_4 ]
+	});
+});
+
+app.get('/3/1', function(request, response){
+    response.json({
+		description: area_b_description,
+		dimension: dimensionB3,
+		proficiency: proficiency_B_3_1
+	});
+});
+
+app.get('/3/2', function(request, response){
+    response.json({
+		description: area_b_description,
+		dimension: dimensionB3,
+		proficiency: proficiency_B_3_2
+	});
+});
+
+app.get('/3/3', function(request, response){
+    response.json({
+		description: area_b_description,
+		dimension: dimensionB3,
+		proficiency: proficiency_B_3_3
+	});
+});
+
+app.get('/3/4', function(request, response){
+    response.json({
+		description: area_b_description,
+		dimension: dimensionB3,
+		proficiency: proficiency_B_3_4
+	});
+});
+
+// -----------------------------------------------------
+
+app.get('/4', function(request, response){
+    response.json({
+		description: area_b_description,
+		dimension: dimensionB4,
+		proficiency: [ proficiency_B_4_1, proficiency_B_4_2, proficiency_B_4_3 ]
+	});
+});
+
+app.get('/4/1', function(request, response){
+    response.json({
+		description: area_b_description,
+		dimension: dimensionB4,
+		proficiency: proficiency_B_4_1
+	});
+});
+
+app.get('/4/2', function(request, response){
+    response.json({
+		description: area_b_description,
+		dimension: dimensionB4,
+		proficiency: proficiency_B_4_2
+	});
+});
+
+app.get('/4/3', function(request, response){
+    response.json({
+		description: area_b_description,
+		dimension: dimensionB4,
+		proficiency: proficiency_B_4_3
+	});
+});
+
+// -----------------------------------------------------
+
+app.get('/5', function(request, response){
+    response.json({
+		description: area_b_description,
+		dimension: dimensionB5,
+		proficiency: [ proficiency_B_5_1, proficiency_B_5_2, proficiency_B_5_3 ]
+	});
+});
+
+app.get('/5/1', function(request, response){
+    response.json({
+		description: area_b_description,
+		dimension: dimensionB5,
+		proficiency: proficiency_B_5_1
+	});
+});
+
+app.get('/5/2', function(request, response){
+    response.json({
+		description: area_b_description,
+		dimension: dimensionB5,
+		proficiency: proficiency_B_5_2
+	});
+});
+
+app.get('/5/3', function(request, response){
+    response.json({
+		description: area_b_description,
+		dimension: dimensionB5,
+		proficiency: proficiency_B_5_3
+	});
+});
+
+// -----------------------------------------------------
+
+app.get('/6', function(request, response){
+    response.json({
+		description: area_b_description,
+		dimension: dimensionB6,
+		proficiency: [ proficiency_B_6_3, proficiency_B_6_4 ]
+	});
+});
+
+app.get('/6/3', function(request, response){
+    response.json({
+		description: area_b_description,
+		dimension: dimensionB6,
+		proficiency: proficiency_B_6_3
+	});
+});
+
+app.get('/6/4', function(request, response){
+    response.json({
+		description: area_b_description,
+		dimension: dimensionB6,
+		proficiency: proficiency_B_6_4
+	});
+});
+
+// -----------------------------------------------------
 
 // exporteer alle express app routes, en de variabele area_b.
 // De variabele komt beschikbaar onder 'info'.
